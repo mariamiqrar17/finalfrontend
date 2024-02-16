7ad1fc7ee61a7b581d58c946b3f44e039bd47a27
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-02-16 18:09:01 +0500

    change

---
+++ components/Feeds.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  ListGroup,
+  CardSubtitle,
+  ListGroupItem,
+  Button,
+} from "reactstrap";
+
+interface News {
+  title: string;
+  color: string;
+  date: string;
+  id: number;
+  author: string;
+}
+
+const Feeds: React.FC = () => {
+  const [feeds, setFeeds] = useState<News[]>([]);
+
+  useEffect(() => {
+    const fetchNewsByAuthor = async () => {
+      try {
+        const response = await axios.get("/api/news/author");
+        setFeeds(response.data);
+      } catch (error) {
+        console.error("Error fetching news by author:", error);
+      }
+    };
+
+    fetchNewsByAuthor();
+  }, []);
+
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle tag="h5">Feeds</CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+          Widget you can use
+        </CardSubtitle>
+        <ListGroup flush className="mt-4">
+          {feeds.map((feed) => (
+            <ListGroupItem
+              key={feed.author}
+              action
+              href="/"
+              tag="a"
+              className="d-flex align-items-center p-3 border-0"
+            >
+              <Button
+                className="rounded-circle me-3"
+                size="sm"
+                color={feed.color}
+              >
+              </Button>
+              {feed.title}
+              <small className="ms-auto text-muted text-small">
+                {feed.date}
+              </small>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default Feeds;
---
+++ components/SalesChart.tsx
@@ -0,0 +1,66 @@
+'use client'
+import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const SalesChart = () => {
+  const chartoptions = {
+    series: [
+      {
+        name: "",
+        data: [10, 31, 87, 28, 51, 42],
+      },
+      {
+        name: "",
+        data: [40, 47, 32, 45, 32, 34],
+      },
+    ],
+    options: {
+      chart: {
+        type: "area",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      grid: {
+        strokeDashArray: 3,
+        borderColor: "rgba(0,0,0,0.9)",
+      },
+
+      stroke: {
+        curve: "smooth",
+        width: 1,
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "March",
+          "April",
+          "May",
+          "June",
+        ],
+      },
+    },
+  };
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle tag="h5">Sales Summary</CardTitle>
+        <CardSubtitle className="text-muted" tag="h6">
+          
+        </CardSubtitle>
+        <Chart
+          type="area"
+          width="100%"
+          height="390"
+          options={chartoptions.options}
+          series={chartoptions.series}
+        />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default SalesChart;
